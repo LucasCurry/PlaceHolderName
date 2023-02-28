046a8fcf7e07e7af9f2bf02b54b75462cf19a6fc
Eira Birkhammar <eirabirkhammar@gmail.com>
2023-02-28 11:54:37 +0100

    Eira did a bunch of shit

--- Grupp 1/src/main/java/com/example/demo/Dog.java
+++ Grupp 1/src/main/java/com/example/demo/Dog.java
@@ -9,5 +9,70 @@ public class Dog {
     private Breed breed;
     private String description;
 
+    public Dog(int age, String name, AdoptionStatus status, AgeCategory ageCategory, String color, Breed breed) {
+        this.age = age;
+        this.name = name;
+        this.status = status;
+        this.ageCategory = ageCategory;
+        this.color = color;
+        this.breed = breed;
+        this.description = "I am a dog, woof woof!";
+    }
 
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public AdoptionStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AdoptionStatus status) {
+        this.status = status;
+    }
+
+    public AgeCategory getAgeCategory() {
+        return ageCategory;
+    }
+
+    public void setAgeCategory(AgeCategory ageCategory) {
+        this.ageCategory = ageCategory;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Breed getBreed() {
+        return breed;
+    }
+
+    public void setBreed(Breed breed) {
+        this.breed = breed;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
