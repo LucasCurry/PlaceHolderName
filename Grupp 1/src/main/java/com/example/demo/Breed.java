386a0a9930b4f3ead34558772eb7b11efce0fb03
LucasCurry <123386797+LucasCurry@users.noreply.github.com>
2023-02-28 11:36:28 +0100

    Add files via upload

---
+++ Grupp 1/src/main/java/com/example/demo/Breed.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public enum Breed {
+    STAFFORDSHIRE_TERRIER("StaffordShire Terrier"),
+
+    ;
+    Breed(String value) {
+    }
+}
+
---
+++ Grupp 1/src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class Dog {
+    private int age;
+    private String name;
+    private AdoptionStatus status;
+    private AgeCategory ageCategory;
+    private String color;
+    private Breed breed;
+    private String description;
+
+
+}
