01ca8e32d36c9defa8af77708cbd51a5e877cb27
LucasCurry <123386797+LucasCurry@users.noreply.github.com>
2023-03-02 17:36:09 +0100

    Add files via upload

--- PlaceHolderName-master/src/main/java/com/example/demo/DemoController.java
+++ PlaceHolderName-master/src/main/java/com/example/demo/DemoController.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -14,7 +17,9 @@ public class DemoController {
 
     @Autowired
     private DogRepository dogRepository;
-    private static final int PAGE_SIZE = 5;
+    @Autowired
+    private DonationShop donationShop;
+    private static final int PAGE_SIZE = 8;
 
     @GetMapping("/")
     public String index(Model model) {
@@ -36,12 +41,35 @@ public class DemoController {
         return "dogs";
     }
     @GetMapping("/dog/{id}")
-    public String book(Model model, @PathVariable Integer id) {
+    public String dog(Model model, @PathVariable Integer id) {
         Dog dog = dogRepository.getDog(id);
         model.addAttribute("dog", dog);
 
         return "dog";
     }
+
+    @GetMapping("/donations")
+    public String donations(Model model, DonationShop donationShop){
+        List<Item> itemList = donationShop.getAllItems();
+        model.addAttribute("itemList", itemList);
+
+        return "donations";
+    }
+
+    @GetMapping("/item/{id}")
+    public String item(Model model, @PathVariable Integer id) {
+        Item item = donationShop.getItem(id);
+        model.addAttribute("item", item);
+
+        return "item";
+    }
+
+    @PostMapping("/item/{id}")
+    public String level1post(Model model, @PathVariable Integer id, @RequestParam String item){
+
+        return "item";
+    }
+
     private int[] toArray(int num) {
         int[] result = new int[num];
         for (int i = 0; i < num; i++) {
